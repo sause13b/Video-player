bd838260d88a036bf66742e3c74960630421730a
Your Name <Pavlich_eng00@mail.ru>
2021-06-25 23:31:09 +1000

    get YT video ID

--- static/video/video.js
+++ static/video/video.js
@@ -4,14 +4,25 @@ window.onload = function() {
         let url = document.getElementById('videoinput')
         let btn = document.getElementById('submitbuton')
         let list = document.getElementById('list')
+        let player_src = 'https://youtu.be/2mb2Y8iYWBs'
         btn.addEventListener('click', function (){
-            arr.push(url.value)
-            let li = document.createElement('li');
-            li.textContent = url.value;
-            list.appendChild(li);
+            let i = 0
+            let buf = url.value
+
+                buf = buf.slice(32);
+                arr.push(buf)
+                let li = document.createElement('li');
+                li.textContent = buf;
+                list.appendChild(li);
+
+
         })
-        let player = document.getElementById('player')
-        player.src="https://youtu.be/2mb2Y8iYWBs"
+
+
+
+
+
+
 
 
     }
